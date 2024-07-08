de8333c4ec60ee83d4d16ce353b476e098f4d587
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-07-08 10:06:26 +0300

    PUBLISH

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <=400) {
+    return 'This one is on me!';
+  }
+  else if (ride > 400 && ride <=2000){
+    return 'That will be twenty bucks.';
+  }else if (ride > 2000 && ride <=2500){
+    return 'I will gladly take your thirty bucks.';
+  }else if(ride > 2500){
+    return 'No can do.';
+  }
+    
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  return tip === 'generous' ? 'Thank you so much.' : tip === 'not as generous' ? 'Thank you.' : 'Bye.';
 }
